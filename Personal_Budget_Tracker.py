921c2a8870ac416e4a362637612a923872f9e554
Heba Wali <26.hebawali@gmail.com>
2026-02-21 17:58:13 -0600

    updated budget function. not done, will update la ter

--- Personal_Budget_Tracker.py
+++ Personal_Budget_Tracker.py
@@ -4,6 +4,28 @@ print("Hello World!")
 # HELLO EVERYONE PLEASE PUT YOUR CODE UNDER THIS LINE
 
 
+# Budget Managment Functions
+# Function to Set a Monthly Budget
+def setting_budget(budget_limits):
+    print(" --- Set Monthly Budget ---")
+    print("Categories: Food, Transportation, Entertainment, Bills")
+    
+    # Asking the user which category they want to set a budget for, and storing it.
+    category = input("Please enter the category you want to set a budget for: ").capitalize()
+    
+    # Checking if the category exists in the dictionary
+    if category in budget_limits:
+        try:
+            # Ask for the limit and convert/store it as a float.
+            amount = float(input(f"Enter the budget amount for {category}: "))
+            budget_limits[category] = amount
+            print(f"Adding the budget was successful! The monthly budget for {category} is now ${amount:.2f}")
+        except ValueError:
+            print("Invalid input. Please try again and enter a numeric value for the budget amount.")
+    else:
+        print("That category does not exist. Please try again.")
+            
+
 
 # Displaying the Main Menu and calling the functions based on what the user enters.
 def main():
@@ -11,7 +33,7 @@ def main():
     all_transactions = []
     
     # Setting monthly budget limits for expense categories.
-    # Setting 0 as a placeholder. (DECIDE TOGETHER ON BUDGET)
+    # Setting 0 as a placeholder. Changes when user inputs something else.
     budget_limits = {"Food": 0, "Transportation": 0, "Entertainment": 0, "Bills": 0}
     
     # Main Menu System
@@ -22,7 +44,7 @@ def main():
         print("    1. Add Income")
         print("    2. Add Expense")
         print("    3. View All Transactions")
-        print("    4. Set Budget")
+        print("    4. Set a Monthly Budget")
         print("    5. View Budget Summary")
         print("    6. Generate Report")
         print("    7. Save and Exit")
@@ -39,6 +61,7 @@ def main():
             
         elif option == "4":
             print("\n SETTING THE MONTHLY? BUDGET FUNCTION WILL BE CALLED (HEBA)")
+            setting_budget(budget_limits)
             
         elif option == "5":
             print("\n VIEWING BUDGET SUMMARY FUNCTION WILL BE CALLED (HEBA)")
